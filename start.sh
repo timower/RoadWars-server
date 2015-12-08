e670f6014216ff38447d1e52dba822d990d161e4
timower <timothy@werquin.com>
2015-12-08 19:04:21 +0100

    fixed some color things

--- start.sh
+++ start.sh
@@ -2,11 +2,13 @@
 
 echo "killing server:"
 screen -ls | grep roadwars | cut -d. -f1 | awk '{print $1}' | xargs kill
+screen -ls | grep web | cut -d. -f1 | awk '{print $1}' | xargs kill
 echo "done"
 
 echo "starting RoadWars server:"
 screen -dmS roadwars python3 server.py
 echo "OK"
+sleep 1
 echo "starting web server:"
 screen -dmS web python3 web_server.py
 echo "OK"
